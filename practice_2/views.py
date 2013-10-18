549feddc4cf63c3f63e1b78315f6911e16570306
Dmitry Shishmakov <dmitry.shishmakov@gmail.com>
2013-10-18 14:11:44 -0700

    Update views.py

--- practice_2/views.py
+++ practice_2/views.py
@@ -1,14 +1,39 @@
-# -*- coding: utf8 -*-
-from os import listdir
-from django.http import HttpResponse
+# Задание: Представление, работающее по разным url
+# # -*- coding: utf8 -*-
+# from os import listdir
+# from django.http import HttpResponse
 
-def listing(request, param):
-		content = ""
-		if(param == 'apt/'):
-			items = listdir("/var/log/apt/")
-		else:
-			items = listdir("/var/log/")
+# def listing(request, param):
+# 		content = ""
+# 		if(param == 'apt/'):
+# 			items = listdir("/var/log/apt/")
+# 		else:
+# 			items = listdir("/var/log/")
 
-		for item in items:
-			content += item + "<br />"
-		return HttpResponse(content)
+# 		for item in items:
+# 			content += item + "<br />"
+# 		return HttpResponse(content)
+
+
+# Задание: Ссылки на подкаталоги
+# # -*- coding: utf8 -*-
+# from os import listdir
+# from os.path import isfile
+# from django.shortcuts import render
+
+# def listing(request, param):
+# 		listFiles = []
+# 		listDirs = []
+# 		path = "/var/log/" + param
+# 		items = listdir(path)
+
+# 		for item in items:
+# 			if(isfile(path + "/" + item)):
+# 				listFiles.append(item)
+# 			else:
+# 				listDirs.append(item)
+
+# 		context = {'listDirs': listDirs, 'listFiles' : listFiles}
+# 		return render(request, 'listing.html', context)
+
+
